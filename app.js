7ea8a8ac6347d111d8d3443f5bea0b4e086e0149
CHATTCBL\Dylan.Rainwater <dylan.rainwater@cblproperties.com>
2017-07-10 12:25:26 -0400

    Plan skeleton of program

---
+++ app.js
@@ -0,0 +1,7 @@
+// Problem: Need a simple way to look at user's badge count & JS points on Treehouse
+// Solution: Use Node.js to connect to Treehouse's API to get and print profile info
+
+// Connect to the API URL (https://teamtreehouse.com/<user>.json)
+// Read data
+// Parse data
+// Print data
